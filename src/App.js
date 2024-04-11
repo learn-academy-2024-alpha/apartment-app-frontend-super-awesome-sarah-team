c0bc694f4344d304319154f0c0c56c1f534833ee
Sarah Proctor <sproctor950@gmail.com>
2024-04-11 10:04:01 -0700

    :sparkles: adds not found component, styles, routing, and passing tests

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { Routes, Route } from "react-router-dom"
 import "./App.css"
 import Footer from "./components/Footer"
 import Header from "./components/Header"
+import Home from "./pages/Home.js"
+import NotFound from "./pages/NotFound.js"
+
 import mockApartments from "./mockApartments.js"
 import mockUsers from "./mockUsers.js"
 
@@ -13,6 +16,10 @@ const App = () => {
   return (
     <>
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
       <Footer />
     </>
   )
---
+++ src/pages/NotFound.js
@@ -0,0 +1,21 @@
+import React from "react"
+import NavButton from "../components/NavButton"
+import key from "../assets/key.png"
+
+const NotFound = () => {
+  return (
+    <div className="center-content page-height">
+      <h2 className="title-header">Oops, Wrong Address!</h2>
+      <NavButton url="/" buttonContent="Back to Listings" />
+      <div>
+        <img
+          src={key}
+          alt="gold skeleton key"
+          className="not-found-key-image"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default NotFound
