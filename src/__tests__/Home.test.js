f7ff12980f8e811663a64f8f6aa3cb1c0db695eb
Sarah Proctor <sproctor950@gmail.com>
2024-04-11 10:03:29 -0700

    :sparkles: adds home component, routings, styles and passing tests

---
+++ src/__tests__/Home.test.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { render, screen } from "@testing-library/react"
+import { BrowserRouter } from "react-router-dom"
+import Home from "../pages/Home"
+import apartments from "../assets/jan-jakub-nanista-unsplash.jpg"
+
+test("renders Home component", () => {
+  render(
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
+  )
+
+  const homeTitleText = screen.getByText(/Home Sweet Home/)
+  expect(homeTitleText).toBeInTheDocument()
+
+  const navButton = screen.getByText(/See Listings/)
+  expect(navButton).toBeInTheDocument()
+
+  const heroImage = screen.getByAltText(
+    "tiled pattern of blue windows on a tan building"
+  )
+  expect(heroImage).toBeInTheDocument()
+  expect(heroImage).toHaveAttribute("src", apartments)
+})
---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import NavButton from "../components/NavButton"
+import apartments from "../assets/jan-jakub-nanista-unsplash.jpg"
+
+const Home = () => {
+  return (
+    <div className="center-content">
+      <h1 className="title-header">Home Sweet Home.</h1>
+      <NavButton url="/" buttonContent="See Listings" />
+      <img
+        src={apartments}
+        alt="tiled pattern of blue windows on a tan building"
+        className="home-apartment-image"
+      />
+    </div>
+  )
+}
+
+export default Home
